425dff6e694dab7da759fec89d045ff36cd61731
Hugo Brosnahan <101022007+hugo-t-b@users.noreply.github.com>
2024-07-02 15:47:38 +1000

    📚 docs: document all direct exports

--- index.ts
+++ index.ts
@@ -6,24 +6,42 @@ import createAdverb, { type AdverbShape as Adverb, type AdverbOptions } from "./
 import createNoun, { type NounShape as Noun, type NounOptions } from "./nouns";
 import createVerb, { type VerbShape as Verb, type VerbOptions } from "./verbs";
 
+/** General type for a word returned from {@link scriptor}. */
 type Word = Adjective | Adverb | Noun | Verb;
+
+/** General type for the options that {@link scriptor} optionally takes as its second argument. */
 type Options = AdjectiveOptions | AdverbOptions | NounOptions | VerbOptions;
 
 export type { Word, Adjective, Adverb, Noun, Verb, Options };
 
 /**
- * Conjugate or decline a word. `scriptor` will automatically detect its part of speech.
+ * Conjugates or declines a word. The part of speech will be automatically detected.
  * 
  * @param parts The principal parts of the word as a string or array.
  * @param options Additional options, such as [overrides](./README.md#overrides) or options specific to the part of speech (optional).
  * @returns An object with every supported form of the word.
  * 
+ * @example Basic usage
  * ```ts
- * const noun = scriptor("scriptor, scriptoris, m");
- * console.log(noun.nominative.plural); //=> "scriptores"
+ * const declined = scriptor("scriptor, scriptoris, m");
+ * console.log(declined.nominative.plural); //=> "scriptores"
+ * ```
+ * Other forms of _scriptor_ can also be accessed as properties of `declined`.
+ * 
+ * @example Specifying options
+ * ```ts
+ * const declined = scriptor("ars, artis, f", { iStem: true });
+ * console.log(declined.genitive.plural); //=> "artium"
+ * ```
+ * 
+ * To correctly decline the i-stem noun _ars_, the `options` argument is needed to specify that it is i-stem.
+ * 
+ * @example Specifying the return type
+ * ```ts
+ * const noun = scriptor<Noun>("scriptor, scriptoris, m");
  * ```
  */
-export default <PartOfSpeech extends Word>(parts: string | string[], options?: Options): PartOfSpeech => {
+const scriptor = <PartOfSpeech extends Word>(parts: string | string[], options?: Options): PartOfSpeech => {
   const [ partOfSpeech, principalParts ] = parsePrincipalParts(parts);
 
   switch (partOfSpeech) {
@@ -38,4 +56,6 @@ export default <PartOfSpeech extends Word>(parts: string | string[], options?: O
     default:
       assertNever(partOfSpeech);
   }
-}
+};
+
+export default scriptor;
